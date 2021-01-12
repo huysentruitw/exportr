77c645943316323af924851d13b3f64c61fbcf4d
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2021-01-12 19:14:18 +0100

    Upgrade packages + copyright year

--- src/Exportr.OpenXml/ExcelDocument.cs
+++ src/Exportr.OpenXml/ExcelDocument.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2017 - 2020 Wouter Huysentruit, Jeroen Verhaeghe
+ * Copyright 2017 - 2021 Wouter Huysentruit, Jeroen Verhaeghe
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
